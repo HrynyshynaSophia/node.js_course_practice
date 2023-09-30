4a0a4796d6467fb0548899eb546cc1f8b6adbd85
Hrynyshyna Sophia <hrunushunasophia13@gmail.com>
2023-09-30 22:03:42 +0300

    errors with api docs fixed

--- src/index.js
+++ src/index.js
@@ -2,16 +2,47 @@ const express = require('express');
 const { specs, swaggerUi } = require('../swagger');
 const app = express();
 const PORT = 3000;
+/**
+ * @swagger
+ * /api-docs
+ */
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 app.get('/', (req, res) => {
   res.send('Welcome to the root path!');
 });
+/**
+ * @swagger
+ * /health-check:
+ *   get:
+ *     summary: Check if the server is running
+ *     responses:
+ *       200:
+ *         description: Server is running
+ */
 app.get('/health-check', (req, res) => {
     res.json({ status: 'Server is running!' });
 });
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Get a list of users
+ *     responses:
+ *       200:
+ *         description: List of users
+ */
 app.get('/api/users',(req,res)=>{
     res.json({message: 'List of users'})
 })
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: Create a new user
+ *     responses:
+ *       200:
+ *         description: User created
+ */
 app.set('/api/users',(req,res)=>{
     res.json({message: 'User created'})
 })
