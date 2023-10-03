188b0235d70011b56fa08efc6fe268c5e36ea029
Hrynyshyna Sophia <hrunushunasophia13@gmail.com>
2023-10-03 09:36:11 +0300

    added jsdoc comments

--- swagger.js
+++ swagger.js
@@ -9,60 +9,7 @@ const options = {
             description: 'Documentation for your API',
         },
     },
-    apis: ['src/*.js'],
-    paths: {
-        '/health-check': {
-            get: {
-                summary: 'Check if the server is running',
-                responses: {
-                    '200': {
-                        description: 'Server is running',
-                    },
-                },
-            },
-        },
-        '/api/users': {
-            get: {
-                summary: 'Get list of users',
-                responses: {
-                    '200': {
-                        description: 'Successful response',
-                    },
-                    '500': {
-                        description: 'Internal server error'
-                    }
-                },
-            },
-            post: {
-                summary: 'Add a new user',
-                responses: {
-                    '201': {
-                        description: 'Successful operation',
-                    },
-                    '400': {
-                        description: 'Bad request'
-                    },
-                    '500': {
-                        description: 'Internal server error'
-                    }
-                },
-            },
-        },
-    },
-    responses: {
-        '200': {
-        description: 'Successful responce'
-        },
-        '400': {
-            description: 'Bad request'
-        },
-        '404': {
-            description: 'Not found'
-        },
-        '500': {
-            description: 'Internal server error'
-        },
-    }
+    apis: ['./src/*.js'],
 }
 
     const specs = swaggerJsdoc(options);
